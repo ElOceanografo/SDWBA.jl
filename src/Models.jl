b2e8b4671db60174a074ff782d01dc9c78858d06
Sam Urmy <samuel.urmy@stonybrook.edu>
2015-09-24 18:04:24 -0400

    had conti/mcgeehee docstrings reversed

--- src/Models.jl
+++ src/Models.jl
@@ -13,14 +13,14 @@ DATA_DIR = joinpath(dirname(@__FILE__), "data/")
 
 
 """
-Krill shape from Conti and Demer, 2006, 'Improved parameterization of the SDWBA for 
-estimating krill target strength.' ICES Journal of Marine Science 63(5), 928-935.
+Krill shape from McGeehee et al. 1998, 'Effects of orientation on acoustic scattering
+from Antarctic krill at 120 kHz.' Deep-Sea Research II 45(7), 1273-1294.
 """
 krill_mcgeehee = from_csv(joinpath(DATA_DIR, "generic_krill_McGeehee1998.csv"))
 
 """
-Krill shape from McGeehee et al. 1998, 'Effects of orientation on acoustic scattering
-from Antarctic krill at 120 kHz.' Deep-Sea Research II 45(7), 1273-1294.
+Krill shape from Conti and Demer, 2006, 'Improved parameterization of the SDWBA for 
+estimating krill target strength.' ICES Journal of Marine Science 63(5), 928-935.
 """
 krill_conti = from_csv(joinpath(DATA_DIR, "generic_krill_Conti2006.csv"))
 
