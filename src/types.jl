40fc06b6cdd87dab970180abb107644c2940a631
Sam Urmy <samuel.urmy@stonybrook.edu>
2015-09-24 10:39:53 -0400

    change dict syntax to tuples from =>

--- src/types.jl
+++ src/types.jl
@@ -199,8 +199,8 @@ names and the values are the actual ones in the file.
 
 `f0` : Standard or verified frequency for the scatterer.  Defaults to 1.0.
 """ ->
-function from_csv(filename, columns=Dict("x"=>"x","y"=>"y","z"=>"z", 
-		"a"=>"a", "h"=>"h", "g"=>"g"); f0=1.0)
+function from_csv(filename, columns=Dict([("x","x"),("y","y"),("z","z"), 
+		("a","a"), ("h","h"), ("g","g")]); f0=1.0)
 	data, header = readdlm(filename, ',', header=true)
 	x = data[:, header .== columns["x"]]
 	y = data[:, header .== columns["y"]]
